73b21d111769ced62d726ee454759073573ac856
sdrty53 <ksjceltonspinks@hotmail.com>
2024-05-06 10:03:36 +0700

    add more features

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function sortedArrayToBST(nums) {
-  if (nums.length === 0) return null;
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-  return root;
+function countConsonants(str) {
+  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
 }
