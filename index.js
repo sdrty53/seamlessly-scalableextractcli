710a756d88fee3e7661230f95c057400fd752271
sdrty53 <ksjceltonspinks@hotmail.com>
2024-05-13 09:22:48 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function kthSmallest(matrix, k) {
-  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
-  flatten.sort((a, b) => a - b);
-  return flatten[k - 1];
+function countAndSay(n) {
+  let result = "1";
+  for (let i = 1; i < n; i++) {
+    let temp = "";
+    let count = 1;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j + 1]) {
+        count++;
+      } else {
+        temp += count + result[j];
+        count = 1;
+      }
+    }
+    result = temp;
+  }
+  return result;
 }
